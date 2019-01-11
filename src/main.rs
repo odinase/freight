dd878689dd5328e446f822c6ee7b63b852d4f3a6
Odin Aleksander Severinsen <odin@severinsen.no>
2019-01-11 14:47:35 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg};
+use std::process::Command;
+
+fn main() {
+    let args = clap_app!(Freight =>
+    (version: crate_version!())
+    (author: "Odin Aleksander Severinsen <odin.sev@gmail.com>")
+    (about: "Cargo clone for C/C++")
+    (@arg fltk: -f --fltk "Sets up the project as FLTK")
+    (@subcommand new =>
+        (@arg project_name: index(1) "Custom name of project")
+        (@arg FLTK: --fltk "Sets up the project as FLTK")
+    )
+    )
+    .get_matches();
+
+    if (args.)
+}
+//
+//let args = clap_app!(Duma =>
+//        (version: crate_version!())
+//        (author: "Matt Gathu <mattgathu@gmail.com>")
+//        (about: "wget clone written in Rust")
+//        (@arg quiet: -q --quiet "quiet (no output)")
+//        (@arg continue: -c --continue "resume getting a partially-downloaded file")
+//        (@arg FILE: -O --output +takes_value "write documents to FILE")
+//        (@arg AGENT: -U --useragent +takes_value "identify as AGENT instead of Duma/VERSION")
+//        (@arg SECONDS: -T --timeout +takes_value "set all timeout values to SECONDS")
+//        (@arg URL: +required +takes_value "url to download")
+//        ).get_matches();
