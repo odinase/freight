6336f3e5b159b54fa9b173a8b3afa47c8ec7f9b3
Odin Aleksander Severinsen <odin@severinsen.no>
2019-01-12 09:46:18 +0100

    Mostly testing of stuff, manage to get path to execution

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ extern crate clap;
 extern crate freight;
 
 use freight::new;
-use std::process::Command;
+use std::env;
 
 fn main() {
+    println!(
+        "Current working directory: {}",
+        env::current_dir().unwrap().to_str().unwrap()
+    );
     let args = clap_app!(Freight =>
     (version: crate_version!())
     (author: "Odin Aleksander Severinsen <odin.sev@gmail.com>")
@@ -15,22 +19,11 @@ fn main() {
         (@arg project_name: index(1) "Custom name of project")
         (@arg FLTK: --fltk "Sets up the project as FLTK")
     )
-    ).get_matches();
+    )
+    .get_matches();
 
-    match args.subcommand() {
-        ("new", Some(sub_args)) => new::create_project(sub_args),
-        _ => (),
-    }
+    //println!("{:?}", args);
+
+    let (_, sub_args) = args.subcommand();
+    new::create_project(sub_args.unwrap());
 }
-//
-//let args = clap_app!(Duma =>
-//        (version: crate_version!())
-//        (author: "Matt Gathu <mattgathu@gmail.com>")
-//        (about: "wget clone written in Rust")
-//        (@arg quiet: -q --quiet "quiet (no output)")
-//        (@arg continue: -c --continue "resume getting a partially-downloaded file")
-//        (@arg FILE: -O --output +takes_value "write documents to FILE")
-//        (@arg AGENT: -U --useragent +takes_value "identify as AGENT instead of Duma/VERSION")
-//        (@arg SECONDS: -T --timeout +takes_value "set all timeout values to SECONDS")
-//        (@arg URL: +required +takes_value "url to download")
-//        ).get_matches();
--- src/new.rs
+++ src/new.rs
@@ -1,5 +1,37 @@
 use clap::ArgMatches;
+use std::process::Command;
 
 pub fn create_project(args: &ArgMatches) {
-    println!("Heyhey!");
+    println!("project_name: {}", args.value_of("project_name").unwrap());
+    Command::new("touch")
+        .arg("hello.txt")
+        .spawn()
+        .expect("failed to execute process");
+    let os = get_os();
+    println!("Your OS is {}", os);
+    Command::new("echo")
+        .arg("$(pwd)")
+        .spawn()
+        .expect("failed to execute process");
 }
+
+fn get_os() -> String {
+    String::from_utf8_lossy(
+        &Command::new("uname")
+            .output()
+            .expect("failed to execute uname")
+            .stdout,
+    )
+    .to_string()
+}
+
+enum Os {
+    Windows,
+    Linux,
+    Mac,
+}
+//
+//impl Os {
+//    fn new() -> Self {}
+//}
+//
